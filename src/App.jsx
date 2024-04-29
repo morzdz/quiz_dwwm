c0bace7f03eb9cab7e9eb25072570c123ca392ad
Billy <billyouadah@gmail.com>
2024-04-29 15:19:39 +0200

    Ajout du menu burger + routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-
 import './App.css';
-import Accueil from './Accueil/Accueil.jsx';
-import Ressources from './Ressources/Ressources.jsx';
+import Accueil from './components/Accueil/Accueil.jsx';
+import Ressources from './components/Ressources/Ressources.jsx';
 // import { useState } from "react";
-//import Sign_Up from './components/Sign_Up/Sign_Up'
-// import Sign_In from './components/Sign_In/Sign_In'
-import "./App.css";
+import Sign_Up from './components/Sign_Up/Sign_Up'
+import Sign_In from './components/Sign_In/Sign_In'
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import {Routes,Route} from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 
 
 
 function App() {
+  //const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <Accueil />
-    </div>
-    <Ressources />
-  )
-    <>
-      <div>
-        <Header />
-        <h1>Quizz</h1>
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+      <Route path="/" element={<Accueil />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        <Route path="/ressources" element={<Ressources />} />
+        <Route path="/signup" element={<Sign_Up />} />
+        <Route path="/signin" element={<Sign_In />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,50 +1,100 @@
-// import { useState } from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import Button from "@mui/material/Button";
 import logo from "../../assets/logo_dwwm_quizz.png";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../../customTheme";
+import { useMediaQuery } from '@mui/material';
 
 const navigation = [
-  { name: "Accueil", href: "#" },
-  { name: "Dashboard", href: "#" },
-  { name: "Ressources", href: "#" },
-  { name: "Inscription/Connexion", href: "#" },
+  { name: "Accueil", href: "/" },
+  { name: "Dashboard", href: "/dashboard" },
+  { name: "Ressources", href: "/ressources" },
+  { name: "Inscription", href: "/signup" }, 
+  { name: "Connexion", href: "/signin" }, 
 ];
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <AppBar
-        position="static"
-        sx={{ backgroundColor: theme.palette.primary.main }}
-      >
+      <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <div className="logo-container">
             <img src={logo} alt="Logo" className="h-8" />
-          </Typography>
+          </div>
           <Typography
             variant="body1"
             component="div"
-            sx={{ color: theme.palette.accent.main }}
+            sx={{
+              color: theme.palette.accent.main,
+              marginLeft: "90px",
+            }}
           >
             "Web-Dev-Quizz c'est le quizz qui permet de réviser la certification
             de développeur web tout en s'amusant"
           </Typography>{" "}
-          <div>
-            {navigation.map((item) => (
-              <Button
-                key={item.name}
-                href={item.href}
+          {isMobile ? (
+            <div className="mobile-menu">
+              <IconButton
+                size="large"
+                edge="end"
                 color="inherit"
-                sx={{ color: theme.palette.common.white }}
+                aria-label="menu"
+                onClick={toggleMenu}
               >
-                {item.name}
-              </Button>
-            ))}
-          </div>
+                {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
+              </IconButton>
+              {isMenuOpen && (
+                <div className="mobile-menu-items">
+                  {navigation.map((item) => (
+                    <Button
+                      key={item.name}
+                      component={Link}
+                      to={item.href}
+                      color="inherit"
+                      sx={{
+                        color: theme.palette.common.white,
+                        width: '100%',
+                        textAlign: 'center',
+                      }}
+                    >
+                      {item.name}
+                    </Button>
+                  ))}
+                </div>
+              )}
+            </div>
+          ) : (
+            <div style={{ marginLeft: "auto" }}>
+              {navigation.map((item, index) => (
+                <Button
+                  key={item.name}
+                  component={Link}
+                  to={item.href}
+                  color="inherit"
+                  sx={{
+                    color: theme.palette.common.white,
+                    marginRight: index < navigation.length - 1 ? "10px" : 0,
+                  }}
+                >
+                  {item.name}
+                </Button>
+              ))}
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </ThemeProvider>
