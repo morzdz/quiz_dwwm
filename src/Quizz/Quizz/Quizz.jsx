bc12a94d88349a9cfd016767a781612358afbab6
morzdz <manon.drozdz@gmail.com>
2024-05-29 10:34:40 +0200

    modif des composants Quizz et QuizzClassique afin de choisir le nombre de questions grâce au props

--- src/Quizz/Quizz/Quizz.jsx
+++ src/Quizz/Quizz/Quizz.jsx
@@ -1,26 +1,29 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useContext } from 'react';
 import { Button, FormControl, OutlinedInput } from '@mui/material';
-import { QuizzContext } from '../../contexts/QuizContext';
 import AutoEvaluation from '../Quizzperso/AutoEvaluation';
 import './Quizz.css';
+import { QuizzContext } from '../../contexts/QuizContext';
 import { quizz } from '../../data/Questions';
 
-const Quiz = () => {
+const Quiz = ({ chosenNum }) => {
     const { maData, addOrUpdateResponse } = useContext(QuizzContext);
     const { questions } = quizz;
+
+    // Filtrer les questions en fonction de chosenNum
+    const numOfQuestions = questions.slice(0, chosenNum);
 
     const [activeQuestion, setActiveQuestion] = useState(0);
     const [userAnswer, setUserAnswer] = useState('');
     const [showResult, setShowResult] = useState(false);
     const [showValidateButton, setShowValidateButton] = useState(true);
     const [correctAnswer, setCorrectAnswer] = useState('');
-    const [scores, setScores] = useState(Array(questions.length).fill(0));
+    const [scores, setScores] = useState(Array(chosenNum).fill(0));
     const [nextButtonDisabled, setNextButtonDisabled] = useState(true);
     const [selectedEvaluationIndex, setSelectedEvaluationIndex] = useState(-1);
 
     const onClickValidate = () => {
         setShowValidateButton(false);
-        setCorrectAnswer(questions[activeQuestion].correctAnswer);
+        setCorrectAnswer(numOfQuestions[activeQuestion].correctAnswer);
     };
 
     const handleSelectEvaluation = (index) => {
@@ -30,9 +33,9 @@ const Quiz = () => {
 
     const handleScore = (index) => {
         setScores(prevScores => {
-            const newScores = [...prevScores];
-            newScores[activeQuestion] = index;
-            return newScores;
+        const newScores = [...prevScores];
+        newScores[activeQuestion] = index;
+        return newScores;
         });
         setNextButtonDisabled(false);
     };
@@ -41,23 +44,23 @@ const Quiz = () => {
         setSelectedEvaluationIndex(-1);
         setShowValidateButton(true);
         setNextButtonDisabled(true);
-        if (activeQuestion !== questions.length - 1) {
-            setActiveQuestion(prev => prev + 1);
+        if (activeQuestion !== numOfQuestions.length - 1) {
+        setActiveQuestion(prev => prev + 1);
         } else {
-            setShowResult(true);
+        setShowResult(true);
         }
         setUserAnswer('');
         handleResponse();
     };
 
     const handleResponse = () => {
-        const questionId = questions[activeQuestion].question_id;
+        const questionId = numOfQuestions[activeQuestion].question_id;
         const response = {
-            question_id: questionId,
-            user_id: 101,
-            text: userAnswer,
-            evalIndex: selectedEvaluationIndex,
-            questions: questions[activeQuestion]
+        question_id: questionId,
+        user_id: 101,
+        text: userAnswer,
+        evalIndex: selectedEvaluationIndex,
+        questions: numOfQuestions[activeQuestion]
         };
 
         console.log("Nouvelle réponse :", response);
@@ -68,115 +71,115 @@ const Quiz = () => {
     const addLeadingZero = (number) => (number > 9 ? number : `0${number}`);
 
     const userScore = scores.reduce((acc, score) => acc + score, 0);
-    const maxScore = 2 * questions.length;
+    const maxScore = 2 * numOfQuestions.length;
 
     return (
         <div className="quiz-container">
-            {!showResult ? (
-                <div>
-                    <div style={{ width: '1em', position: 'relative', top: '8px', left: '8px' }}>
-                        <span>{addLeadingZero(activeQuestion + 1)}</span>
-                        <span>/{addLeadingZero(questions.length)}</span>
-                    </div>
-                    <h2>{questions[activeQuestion].question}</h2>
-                    <div style={{ marginBottom: '1rem' }}>
-                        Question {activeQuestion + 1} sur {questions.length}
-                    </div>
-                    <FormControl id='input' style={{ width: '30rem' }}>
-                        <OutlinedInput
-                            autoComplete='off'
-                            id='answer'
-                            type="text"
-                            value={userAnswer}
-                            onChange={(e) => setUserAnswer(e.target.value)}
-                            sx={{ width: '30rem' }}
-                        />
-                    </FormControl>
-                    {showValidateButton && (
-                        <div className="flex-right">
-                            <Button
-                                id='submit'
-                                onClick={onClickValidate}
-                                disabled={!userAnswer.trim()}
-                                variant='outlined'
-                            >
-                                Valider
-                            </Button>
-                        </div>
-                    )}
-                    {!showValidateButton && (
-                        <div>
-                            <h4>Voici la réponse type !</h4>
-                            <div className='answer-container'>
-                                <p>{correctAnswer}</p>
-                            </div>
-                            <p>Comment évaluez-vous votre réponse :</p>
-                            <AutoEvaluation
-                                key={activeQuestion}
-                                selectedEvaluationIndex={selectedEvaluationIndex}
-                                handleSelectEvaluation={handleSelectEvaluation}
-                            />
-                            <div className="flex-right">
-                                <Button
-                                    id='submit'
-                                    onClick={() => onClickNext()}
-                                    disabled={nextButtonDisabled}
-                                    variant='outlined'
-                                >
-                                    {activeQuestion === questions.length - 1 ? 'Terminer' : 'Question suivante'}
-                                </Button>
-                            </div>
-                        </div>
-                    )}
-                </div>
-            ) : (
-                <div className="result">
-                    <h3>Résultat</h3>
-                    <p>
-                        Nombre total de questions : <span>{questions.length}</span>
-                    </p>
-                    <p>Votre score : {userScore} / {maxScore}</p>
-                    <p>Votre résultats en détail :</p>
-                    <ul>
-                        {scores.filter(score => score === 0).length > 0 && (
-                            <li>
-                                <p>Nombre de questions non acquises :</p>
-                                <div style={{ display: 'flex', width: '200px' }}>
-                                    <div
-                                        className="non-acquise"
-                                        style={{ width: `${(scores.filter(score => score === 0).length / questions.length) * 100}%` }}
-                                    ><p className='invisible'>{scores.filter(score => score === 0).length}</p></div>
-                                    {scores.filter(score => score === 0).length}
-                                </div>
-                            </li>
-                        )}
-                        {scores.filter(score => score === 1).length > 0 && (
-                            <li>
-                                <p>Nombre de questions partiellement acquises :</p>
-                                <div style={{ display: 'flex', width: '200px' }}>
-                                    <div
-                                        className="partiellement-acquise"
-                                        style={{ width: `${(scores.filter(score => score === 1).length / questions.length) * 100}%` }}
-                                    ><p className='invisible'>{scores.filter(score => score === 1).length}</p></div>
-                                    {scores.filter(score => score === 1).length}
-                                </div>
-                            </li>
-                        )}
-                        {scores.filter(score => score === 2).length > 0 && (
-                            <li>
-                                <p>Nombre de questions acquises :</p>
-                                <div style={{ display: 'flex', width: '200px' }}>
-                                    <div
-                                        className="acquise"
-                                        style={{ width: `${(scores.filter(score => score === 2).length / questions.length) * 100}%` }}
-                                    ><p className='invisible'>{scores.filter(score => score === 2).length}</p></div>
-                                    {scores.filter(score => score === 2).length}
-                                </div>
-                            </li>
-                        )}
-                    </ul>
+        {!showResult ? (
+            <div>
+            <div style={{ width: '1em', position: 'relative', top: '8px', left: '8px' }}>
+                <span>{addLeadingZero(activeQuestion + 1)}</span>
+                <span>/{addLeadingZero(numOfQuestions.length)}</span>
+            </div>
+            <h2>{numOfQuestions[activeQuestion].question}</h2>
+            <div style={{ marginBottom: '1rem' }}>
+                Question {activeQuestion + 1} sur {numOfQuestions.length}
+            </div>
+            <FormControl id='input' style={{ width: '30rem' }}>
+                <OutlinedInput
+                autoComplete='off'
+                id='answer'
+                type="text"
+                value={userAnswer}
+                onChange={(e) => setUserAnswer(e.target.value)}
+                sx={{ width: '30rem' }}
+                />
+            </FormControl>
+            {showValidateButton && (
+                <div className="flex-right">
+                <Button
+                    id='submit'
+                    onClick={onClickValidate}
+                    disabled={!userAnswer.trim()}
+                    variant='outlined'
+                >
+                    Valider
+                </Button>
                 </div>
             )}
+            {!showValidateButton && (
+                <div>
+                <h4>Voici la réponse type !</h4>
+                <div className='answer-container'>
+                    <p>{correctAnswer}</p>
+                </div>
+                <p>Comment évaluez-vous votre réponse :</p>
+                <AutoEvaluation
+                    key={activeQuestion}
+                    selectedEvaluationIndex={selectedEvaluationIndex}
+                    handleSelectEvaluation={handleSelectEvaluation}
+                />
+                <div className="flex-right">
+                    <Button
+                    id='submit'
+                    onClick={() => onClickNext()}
+                    disabled={nextButtonDisabled}
+                    variant='outlined'
+                    >
+                    {activeQuestion === numOfQuestions.length - 1 ? 'Terminer' : 'Question suivante'}
+                    </Button>
+                </div>
+                </div>
+            )}
+            </div>
+        ) : (
+            <div className="result">
+            <h3>Résultat</h3>
+            <p>
+                Nombre total de questions : <span>{numOfQuestions.length}</span>
+            </p>
+            <p>Votre score : {userScore} / {maxScore}</p>
+            <p>Votre résultats en détail :</p>
+            <ul>
+                {scores.filter(score => score === 0).length > 0 && (
+                <li>
+                    <p>Nombre de questions non acquises :</p>
+                    <div style={{ display: 'flex', width: '200px' }}>
+                    <div
+                        className="non-acquise"
+                        style={{ width: `${(scores.filter(score => score === 0).length / numOfQuestions.length) * 100}%` }}
+                    ><p className='invisible'>{scores.filter(score => score === 0).length}</p></div>
+                    {scores.filter(score => score === 0).length}
+                    </div>
+                </li>
+                )}
+                {scores.filter(score => score === 1).length > 0 && (
+                <li>
+                    <p>Nombre de questions partiellement acquises :</p>
+                    <div style={{ display: 'flex', width: '200px' }}>
+                    <div
+                        className="partiellement-acquise"
+                        style={{ width: `${(scores.filter(score => score === 1).length / numOfQuestions.length) * 100}%` }}
+                    ><p className='invisible'>{scores.filter(score => score === 1).length}</p></div>
+                    {scores.filter(score => score === 1).length}
+                    </div>
+                </li>
+                )}
+                {scores.filter(score => score === 2).length > 0 && (
+                <li>
+                    <p>Nombre de questions acquises :</p>
+                    <div style={{ display: 'flex', width: '200px' }}>
+                    <div
+                        className="acquise"
+                        style={{ width: `${(scores.filter(score => score === 2).length / numOfQuestions.length) * 100}%` }}
+                    ><p className='invisible'>{scores.filter(score => score === 2).length}</p></div>
+                    {scores.filter(score => score === 2).length}
+                    </div>
+                </li>
+                )}
+            </ul>
+            </div>
+        )}
         </div>
     );
 };
--- src/Quizz/QuizzClassique/QuizzClassique.jsx
+++ src/Quizz/QuizzClassique/QuizzClassique.jsx
@@ -1,33 +1,40 @@
-import React from 'react';
+import { useState } from 'react';
 import { Button } from '@mui/material'; 
-import { Link } from "react-router-dom";
+import Quiz from '../Quizz/Quizz';
 import theme from "../../customTheme";
 import { ThemeProvider } from '@mui/material/styles'; 
 import './QuizzClassique.css';
 
 const QuizzClassique = () => {
+
+  const [startQuizz, setStartQuizz] = useState(false);
+  const [chosenNum, setChosenNum] = useState(0);
+
   const userChoice = (nombreQuestions) => {
-    // console.log(`L'utilisateur a choisi ${nombreQuestions} questions`);
+    setChosenNum(nombreQuestions);
+    setStartQuizz(true);
   };
 
   return (
-    <>
-   <ThemeProvider theme={theme}>
-        <div style={{ textAlign: 'center', marginTop: '30px' }}>
-          <h1>Quizz classique</h1>
-          <h2>Choisissez le nombre de questions :</h2>
-          <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '300px', margin: '0 auto', marginBottom: '40px' }}>
-          <Link to="/Quizz">
-            <Button variant="contained" onClick={() => userChoice(20)} style={{ marginBottom: '20px', width: '100%', fontSize: '16px', padding: '10px' }}>20 questions</Button>
-            </Link>
-            <Button variant="contained" onClick={() => userChoice(50)} style={{ marginBottom: '20px', width: '100%', fontSize: '16px', padding: '10px' }}>50 questions</Button>
-            <Button variant="contained" onClick={() => userChoice(100)} style={{ marginBottom: '20px', width: '100%', fontSize: '16px', padding: '10px' }}>100 questions</Button>
-            <Button variant="contained" onClick={() => userChoice(150)} style={{ width: '100%', fontSize: '16px', padding: '10px' }}>150 questions</Button>
-          </div>
-        </div>
-      </ThemeProvider>
-    </>
-  );
+    <ThemeProvider theme={theme}>
+      <div style={{ textAlign: 'center', marginTop: '30px' }}>
+        {!startQuizz ? (
+          <>
+            <h1>Quizz classique</h1>
+            <h2>Choisissez le nombre de questions :</h2>
+            <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '300px', margin: '0 auto', marginBottom: '40px' }}>
+              <Button variant="contained" onClick={() => userChoice(20)} style={{ marginBottom: '20px', width: '100%', fontSize: '16px', padding: '10px' }}>20 questions</Button>
+              <Button variant="contained" onClick={() => userChoice(50)} style={{ marginBottom: '20px', width: '100%', fontSize: '16px', padding: '10px' }}>50 questions</Button>
+              <Button variant="contained" onClick={() => userChoice(100)} style={{ marginBottom: '20px', width: '100%', fontSize: '16px', padding: '10px' }}>100 questions</Button>
+              <Button variant="contained" onClick={() => userChoice(150)} style={{ width: '100%', fontSize: '16px', padding: '10px' }}>150 questions</Button>
+            </div>
+          </>
+        ) : (
+          <Quiz chosenNum={chosenNum} />
+        )}
+      </div>
+    </ThemeProvider>
+  );  
 };
 
 export default QuizzClassique;
